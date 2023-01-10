24c95a41c1ed27ccdf9ef6b47e92f0bd35424526
da4o88 <da4o88@users.noreply.github.com>
2023-01-10 22:19:36 +0100

    basic hmtl and css settings

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask
+from flask import render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
+import sqlite3
+
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data"
+db = SQLAlchemy(app)
+
+
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():  # put application's code here
+    companies = db.Query
+    return render_template("home.html")
+
 
 
 if __name__ == '__main__':
