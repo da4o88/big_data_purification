986efb708b07e38deb86532d4fdda3c0bca22d65
da4o88 <da4o88@users.noreply.github.com>
2023-01-11 00:06:49 +0100

    get raw data from database and display in html table

--- app.py
+++ app.py
@@ -5,19 +5,24 @@ import pandas as pd
 import sqlite3
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data"
-db = SQLAlchemy(app)
+# Get DB connections
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data"
+# db = SQLAlchemy(app)
+
 
 
 @app.route('/')
 def home():  # put application's code here
-    companies = db.Query
+
     return render_template("home.html")
 
 
 @app.route('/raw-data')
 def get_raw_data():
-    return render_template("raw_data.html")
+    conn = get_db_connection()
+    companies = conn.execute('SELECT * FROM companies').fetchall()
+    conn.close()
+    return render_template("raw_data.html", companies=companies)
 
 
 @app.route('/show-data')
@@ -25,5 +30,11 @@ def show_data():
     return render_template("show_data.html")
 
 
+def get_db_connection():
+    conn = sqlite3.connect('data.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
