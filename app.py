d5ae8f870cd2b027c576870b4760b3047736e574
da4o88 <da4o88@users.noreply.github.com>
2023-01-10 22:44:29 +0100

    navbar link connected

--- app.py
+++ app.py
@@ -4,12 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 import sqlite3
 
-
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data"
 db = SQLAlchemy(app)
-
-
 
 
 @app.route('/')
@@ -17,6 +14,15 @@ def home():  # put application's code here
     companies = db.Query
     return render_template("home.html")
 
+
+@app.route('/raw-data')
+def get_raw_data():
+    return render_template("raw_data.html")
+
+
+@app.route('/show-data')
+def show_data():
+    return render_template("show_data.html")
 
 
 if __name__ == '__main__':
