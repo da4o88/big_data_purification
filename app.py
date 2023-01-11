fc3160e50d0ee66b21eb4bc2439388675720d7bf
da4o88 <da4o88@users.noreply.github.com>
2023-01-11 02:15:23 +0100

    store companies data in list

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask
 from flask import render_template, redirect, request
-from flask_sqlalchemy import SQLAlchemy
-import pandas as pd
 import sqlite3
+from flask_paginate import Pagination, get_page_args
 
 app = Flask(__name__)
-# Get DB connections
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data"
-# db = SQLAlchemy(app)
 
+ROWS_PER_PAGE = 10
 
 
 @app.route('/')
@@ -18,10 +15,21 @@ def home():  # put application's code here
 
 
 @app.route('/raw-data')
-def get_raw_data():
-    conn = get_db_connection()
-    companies = conn.execute('SELECT * FROM companies').fetchall()
-    conn.close()
+def show_raw_data():
+    # new_companies = []
+    # companies = get_raw_data()
+    #
+    # for c in companies:
+    #     company = {
+    #         "id": c['id'],
+    #         "name": c['name'],
+    #         "country_iso": c['country_iso'],
+    #         "city": c['city'],
+    #         "nace": c['nace'],
+    #         "website": c['website']
+    #     }
+    #     new_companies.append(company)
+
     return render_template("raw_data.html", companies=companies)
 
 
@@ -35,6 +43,33 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+
+def get_raw_data():
+    conn = get_db_connection()
+    companies_from_db = conn.execute('SELECT * FROM companies').fetchall()
+    conn.close()
+
+    return companies_from_db
+
+
+def get_companies():
+    new_companies = []
+    companies_from_db = get_raw_data()
+
+    for c in companies_from_db:
+        company = {
+            "id": c['id'],
+            "name": c['name'],
+            "country_iso": c['country_iso'],
+            "city": c['city'],
+            "nace": c['nace'],
+            "website": c['website']
+        }
+        new_companies.append(company)
+    return new_companies
+
+
+companies = get_companies()
 
 if __name__ == '__main__':
     app.run(debug=True)
