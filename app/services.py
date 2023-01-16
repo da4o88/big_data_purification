af4d8aa734fa464d527391fbe1456df48a3d7777
da4o88 <da4o88@users.noreply.github.com>
2023-01-16 06:27:32 +0100

    move connect to DB in services file,fixed z-index on footer

--- app/services.py
+++ app/services.py
@@ -1,4 +1,6 @@
 import sqlite3
+from app import app
+from flask_mongoengine import MongoEngine
 
 # Connect to SQLite Database
 
@@ -7,3 +9,15 @@ def get_db_connection():
     conn = sqlite3.connect('data.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+
+# Connect to MongoDB
+
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'company_db',
+    'host': 'localhost',
+    'port': 27017
+}
+
+db = MongoEngine()
+db.init_app(app)
